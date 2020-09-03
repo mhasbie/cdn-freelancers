7c1934690289dd30efa11ee08b0698209ad35491
M. Hasbie <m_hasbie@yahoo.com>
2020-09-03 12:13:08 +0800

    Initial setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Param } from '@nestjs/common';
+import { AppService, FindUserDto, CreateUserDto } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	@Get()
+	getHello(): string {
+		return this.appService.getHello();
+	}
+  
+	@Get('all')
+	findAll(): string {
+		return this.appService.findAll();
+	}
+  
+	@Get(':id')
+	getUser(@Param() params: FindUserDto): string {
+		return this.appService.getUser(params.id);
+	}
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+	getHello(): string {
+		return 'Hello World!';
+	}
+	
+	findAll(): string {
+		return 'All users';
+	}
+	
+	getUser(id): string {
+		return `Get user ${id}`;
+	}
+	
+	updateUser(id): string {
+		return `Update user ${id}`;
+	}
+	
+	deleteUser(id): string {
+		return `Delete user ${id}`;
+	}
+	
+	addUser(): string {
+		return `Add new user`;
+	}
+}
+
+export class FindUserDto {
+	@ApiProperty()
+	id: string;
+}
+
+export class CreateUserDto {
+	@ApiProperty()
+	id: string;
+	
+	@ApiProperty()
+	username: string;
+	
+	@ApiProperty()
+	email: string;
+	
+	@ApiProperty()
+	phone: string;
+	
+	@ApiProperty()
+	skillsets: string;
+	
+	@ApiProperty()
+	hobby: string;
 }
